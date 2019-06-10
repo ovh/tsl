28ba7bad701f4166842c3d5b95383c8dccb15d1e
Alessio Coltellacci <lightplay8@gmail.com>
2019-06-10 16:55:25 +0200

    build(ci): remove boostraper for the tsl.wasm

--- wasm/index.js
+++ wasm/index.js
@@ -1,9 +1,1 @@
-import go from "./wasm_exec.js"
-
-function loadTslWasm(wasmFilePath, callback) {
-    WebAssembly.instantiateStreaming(fetch(wasmFilePath), go.importObject).then(callback);
-}
-
-export default {
-    loadTslWasm,
-}
+// npm package require a main js file
