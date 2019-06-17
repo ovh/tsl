3679244053280b19e8298837c427da9e01dceecb
Alessio Coltellacci <lightplay8@gmail.com>
2019-06-17 15:16:59 +0200

    build(ci): use the TSL version command to set the package.json version

--- cmd/version.go
+++ cmd/version.go
@@ -12,9 +12,11 @@ var (
 	githash   = "HEAD"
 	gitbranch = "master"
 	date      = "1970-01-01T00:00:00Z UTC"
+	rawPrint  = false
 )
 
 func init() {
+	versionCmd.PersistentFlags().BoolVarP(&rawPrint, "raw", "r", false, "print only the version number")
 	RootCmd.AddCommand(versionCmd)
 }
 
@@ -22,8 +24,12 @@ var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Print the version number",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("tsl server version %s %s/%s\n", version, gitbranch, githash)
-		fmt.Printf("tsl server build date %s\n", date)
-		fmt.Printf("go version %s %s/%s\n", runtime.Version(), runtime.GOOS, runtime.GOARCH)
+		if rawPrint {
+			fmt.Print(version)
+		} else {
+			fmt.Printf("tsl server version %s %s/%s\n", version, gitbranch, githash)
+			fmt.Printf("tsl server build date %s\n", date)
+			fmt.Printf("go version %s %s/%s\n", runtime.Version(), runtime.GOOS, runtime.GOARCH)
+		}
 	},
 }
